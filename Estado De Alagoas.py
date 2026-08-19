5c4757dfbb8d33e837e1bbfdfd2b35ebe0632ac0
Marcelo Gabriel <150298238+marcelogabrieleuclidesdasiva@users.noreply.github.com>
2026-08-19 13:24:28 -0300

    Add files via upload

---
+++ Estado De Alagoas.py
@@ -0,0 +1,39 @@
+# Estado De Alagoas Da Python 
+
+for i in range(1):
+    print('Alagoas!')
+
+
+from random import * 
+
+Primeiro = str(input('Estado Alagoas: '))
+Segundo = str(input('Estado Alagoas: '))
+Terceiro = str(input('Estado Alagoas: '))
+Quarto = str(input('Estado Alagoas: '))
+Quinto = str(input('Estado Alagoas: '))
+Sexto = str(input('Estado Alagoas: '))
+Sétimo = str(input('Estado Alagoas: '))
+Oitavo = str(input('Estado Alagoas: '))
+Nono = str(input('Estado Alagoas: '))
+Decimo = str(input('Estado Alagoas: '))
+Decimo_Primeiro = str(input('Estado De Alagoas: '))
+Decimo_Segundo = str(input('Estado De Alagoas: '))
+Decimo_Terceiro = str(input('Estado De Alagoas: '))
+Decimo_Quarto = str(input('Estado De Alagoas: '))
+Decimo_Quinto = str(input('Estado De Alagoas: '))
+Decimo_Sexto = str(input('Estado De Alagoas: '))
+Decimo_Sétimo = str(input('Estado De Alagoas: '))
+Decimo_Oitavo = str(input('Estado De Alagoas: '))
+Decimo_Nono = str(input('Estado De Alagoas: '))
+Vigesimo = str(input('Estado De Alagoas: '))
+Vigesimo_Primeiro = str(input('Estado De Alagoas: '))
+Vigesimo_Segundo = str(input('Estado De Alagoas: '))
+
+
+
+Estado_Alagoas = [Primeiro, Segundo, Terceiro, Quarto, Quinto, Sexto, Sétimo, Oitavo, Nono, Decimo, Decimo_Primeiro, Decimo_Segundo, Decimo_Terceiro, Decimo_Quarto, Decimo_Quinto, Decimo_Sexto, Decimo_Sétimo, Decimo_Oitavo, Decimo_Nono,
+Vigesimo, Vigesimo_Primeiro, Vigesimo_Segundo]
+
+
+print('Cidades De Alagoas São {}'.format(Estado_Alagoas))
+
