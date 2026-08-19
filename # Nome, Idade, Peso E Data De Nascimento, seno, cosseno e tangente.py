7a190da6a9c7a8cc0084faf366a933493c953de9
Marcelo Gabriel <150298238+marcelogabrieleuclidesdasiva@users.noreply.github.com>
2026-08-19 13:21:10 -0300

    Add files via upload

---
+++ # Nome, Idade, Peso E Data De Nascimento, seno, cosseno e tangente.py
@@ -0,0 +1,88 @@
+# Nome, Idade, Peso E Data De Nascimento
+nome = input('Informe O Seu Nome: ')
+idade = int(input('Informe A Sua Idade: '))
+peso = input('Informe O Seu Peso: ')
+data_nascimento = input('Informe A Data De Nascimento: ')
+print('Olá {}, Sua Idade É {}, Seu Peso De {}, Data De Nascimento É {}'.format(nome,idade,peso,data_nascimento))
+
+
+from math import radians, sin, cos, tan
+ângulo = float(input('Informe O ângulo Que Você Deseja Ver De Seno, Cosseno E tangente: '))
+seno = sin(radians(ângulo))
+print('Ângulo De {}, O Resultado Do Seno Ficou Como {:.2F}'.format(ângulo, seno))
+cosseno = cos(radians(ângulo))
+print('Ângulo De {}, O Resultado Do Cosseno Ficou Como {:.2F}'.format(ângulo, cosseno))
+tangente = tan(radians(ângulo))
+print('Ângulo De {}, O Resultado Do Tangente Ficou Como {:.2F}'.format(ângulo, tangente))
+
+
+# Nome, Idade, Peso E Data De Nascimento 
+nome = input('Informe O Seu Nome: ')
+idade = int(input('Informe A Sua Idade: '))
+peso = input('Informe O Seu Peso: ')
+data_nascimento = input('Informe A Data De Nascimento: ')
+print('Olá {}, Sua Idade É {}, Seu Peso De {}, Data De Nascimento É {}'.format(nome,idade,peso,data_nascimento))
+
+
+from math import radians, sin, cos, tan 
+ângulo = float(input('Informe O Ângulo Que Você Deseja Ver De Seno, Cosseno E Tangente: '))
+seno = sin(radians(ângulo))
+print('Ângulo De {}, O Resultado Do Seno Ficou Como {:.2F}'.format(ângulo, seno))
+cosseno = cos(radians(ângulo))
+print('Ângulo De {}, O Resultado Do Cosseno Ficou Como {:.2F}'.format(ângulo, cosseno))
+tangente = tan(radians(ângulo))
+print('Ângulo De {}, O Resultado Do tangente Ficou Como {:.2F}'.format(ângulo, tangente))
+
+
+# Nome, Idade, Peso E Data De Nascimento 
+nome = input('Informe O Seu Nome: ')
+idade = int(input('Informe A Sua Idade: '))
+peso = input('Informe O Seu Peso: ')
+data_nascimento = input('Informe A Data De Nascimento: ')
+print('Olá {}, Sua Idade É {}, Seu Peso De {}, Data De Nascimento É {}'.format(nome,idade,peso,data_nascimento))
+
+
+from math import radians, sin, cos, tan 
+ângulo = float(input('Informe O Ângulo Que Você Deseja Ver De Seno, Cosseno E Tangente: '))
+seno = sin(radians(ângulo))
+print('Ângulo De {}, O Resultado Do Seno Ficou Como {:.2F}'.format(ângulo, seno))
+cosseno = cos(radians(ângulo))
+print('Ângulo De {}, O Resultado Do Cosseno Ficou Como {:.2F}'.format(ângulo, cosseno))
+tangente = tan(radians(ângulo))
+print('Ângulo De {}, O Resultado Do Tangente Ficou Como {:.2F}'.format(ângulo, tangente))
+
+
+# Nome, Idade, Peso E Data De Nascimento 
+nome = input('Informe O Seu Nome: ')
+idade = int(input('Informe A Sua Idade: '))
+peso = input('Informe O Seu Peso: ')
+data_nascimento = input('Informe A Data De Nascimento: ')
+print('olá {}, Sua Idade É {}, Seu Peso De {}, Data De Nascimento É {}'.format(nome,idade,peso,data_nascimento))
+
+
+from math import radians, sin, cos, tan 
+ângulo = float(input('Informe O Ângulo Que Você Deseja Ver De Seno, Cosseno E Tangente: '))
+seno = sin(radians(ângulo))
+print('Ângulo De {}, O Resultado Do Seno Ficou Como {:.2F}'.format(ângulo, seno))
+cosseno = cos(radians(ângulo))
+print('Ângulo De {}, O Resultado Do Cosseno Ficou Como {:.2F}'.format(ângulo, cosseno))
+tangente = tan(radians(ângulo))
+print('Ângulo De {}, O Resultado DO Tangente Ficou Como {:.2F}'.format(ângulo, tangente))
+
+
+# Nome, Idade, Peso E Data De Nascimento 
+nome = input('Informe O Seu Nome: ')
+idade = int(input('Informe A Sua Idade: '))
+peso = input('Informe O Seu Peso: ')
+data_nascimento = input('Informe A Data De Nascimento: ')
+print('Olá {}, Sua Idade É {}, Seu Peso De {}, Data De Nascimento É {}'.format(nome,idade,peso,data_nascimento))
+
+
+from math import radians, sin, cos, tan 
+ângulo = float(input('Informe O Ângulo Que Você Deseja Ver De Seno, Cosseno E Tangente: '))
+seno = sin(radians(ângulo))
+print('Ângulo De {}, O Resultado Do Seno Ficou Como {:.2F}'.format(ângulo, seno))
+cosseno = cos(radians(ângulo))
+print('Ângulo De {}, O Resultado Do Cosseno Ficou Como {:.2F}'.format(ângulo, cosseno))
+tangente = tan(radians(ângulo))
+print('Ângulo De {}, O Resultado Do Tangente Ficou Como {:.2F}'.format(ângulo, tangente))
