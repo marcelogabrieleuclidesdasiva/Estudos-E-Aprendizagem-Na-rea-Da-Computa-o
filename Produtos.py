faebde44ea0c6b60a111930dab1cb9491f5f6ef3
Marcelo Gabriel <150298238+marcelogabrieleuclidesdasiva@users.noreply.github.com>
2026-08-19 13:04:11 -0300

    Add files via upload

---
+++ Produtos.py
@@ -0,0 +1,16 @@
+produtos = [
+    "Mobile MR2",
+    "Iphone",
+    "Ipad",
+    "Airpod",
+    "Macbook",
+    "City",
+    "Lojas",
+    "Shooping Center",
+    "Celular",
+    "Milk Shakes",
+    "Restaurantes"
+]
+
+for i, item in enumerate(produtos):
+    print(i, item)
