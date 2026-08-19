25786d30ab001cdc4fd277379f310196bbf4fca4
Marcelo Gabriel <150298238+marcelogabrieleuclidesdasiva@users.noreply.github.com>
2026-08-19 13:19:00 -0300

    Add files via upload

---
+++ Filmes E Personagens.py
@@ -0,0 +1,34 @@
+# Filmes E Personagens 
+
+for i in range(1):
+    print('Filmes E Personagens!')
+
+filme_1 = str(input('Primeiro: '))
+filme_2 = str(input('Segundo: '))
+filme_3 = str(input('Terceiro: '))
+filme_4 = str(input('Quarto: '))
+filme_5 = str(input('Quinto: '))
+filme_6 = str(input('Sexto: '))
+filme_7 = str(input('Sétimo: '))
+filme_8 = str(input('Oitavo: '))
+filme_9 = str(input('Nono: '))
+filme_10 = str(input('Décimo: '))
+personagens_1 = str(input('Primeiro Personagens: '))
+personagens_2 = str(input('Segundo: '))
+personagens_3 = str(input('Terceiro: '))
+personagens_4 = str(input('Quarto Personagens: '))
+personagens_5 = str(input('Quinto: '))
+personagens_6 = str(input('Sexto: '))
+personagens_7 = str(input('Sétimo: '))
+personagens_8 = str(input('Oitavo: '))
+personagens_9 = str(input('Nono: '))
+personagens_10 = str(input('Décimo: '))
+
+Listas_Filmes_Personagens = [filme_1, filme_2, filme_3, filme_4, filme_5, filme_6, filme_7, filme_8, filme_9, filme_10, personagens_1, personagens_2, personagens_3, personagens_4,  
+personagens_5, personagens_6, personagens_6, personagens_7, personagens_8, personagens_9, personagens_10]
+
+
+print('Filmes E Personagens São {}'.format(Listas_Filmes_Personagens))
+
+
+
